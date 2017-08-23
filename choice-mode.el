a99f409205a9ed0dee3f8fbb5ae7e032c7ec5e32
Christopher Brown <MrLordBrown@users.noreply.github.com>
2017-08-22 22:16:06 -0700

    Housecleaning and Prettification

--- choice-mode.el
+++ choice-mode.el
@@ -1,5 +1,5 @@
 ;;; choice-mode.el --- major mode for editing ChoiceScript files. -*- coding: utf-8; lexical-binding: t; mode: sws-mode -*-
-;; Copyright Â© 2017, by Christopher R. Brown
+;; Copyright © 2017, by Christopher R. Brown
 
 ;; Author: Christopher R. Brown ( mrlordvondoombraun@gmail.com )
 ;; Version: 0.0.1
@@ -15,13 +15,13 @@
 ;;  the Free Software Foundation, either version 3 of the License, or
 ;;  (at your option) any later version.
 
-;;    This program is distributed in the hope that it will be useful,
-;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;    GNU General Public License for more details.
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
 
-;;    You should have received a copy of the GNU General Public License
-;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -29,7 +29,6 @@
 
 ;;; Code:
 
-(require 'highlight-numbers)
 (require 'sws-mode)
  
 (defadvice choice-do-indent-line (around choice-indent-blank () activate)
@@ -57,7 +56,6 @@
     ("^*set .*". font-lock-variable-name-face)
     ("^*temp .*" . font-lock-variable-name-face)
     ("\$[\{|\\a-z|\\0-9|\\_|\}]*" . font-lock-variable-name-face)
-;;  ("\#[\\a-z|\\A-Z|\\0-9| |\\.|\\?|\\\"|\\-|\\_|\\!|\\$|\\%|\\'|\{|\}]*\n" . font-lock-constant-face)
     ("^\*page_break\n" . font-lock-warning-face)
     ("\*if" . font-lock-function-name-face)
     ("\*else" . font-lock-function-name-face)
@@ -67,42 +65,14 @@
     ("\*input_text [a-z|A-Z]*\n" . font-lock-builtin-face)
     "Highlight top-level keywords"))
 
-
-;;(defvar choice-mode-syntax-table nil
-;;  "set st for choice-mode")
-
-;;(setq choice-mode-syntax-table
-;;      (let ((synTable (make-syntax-table)))
-;;	(modify-syntax-entry ?* "w" synTable)
-;;	synTable))
-;;(setq choice-keywords '("*title" "*author" "*scene_list" "*choice"))
-
-;;(setq choice-keywords-regexp (regexp-opt choice-keywords 'words))
-
-;;(setq choice-font-lock-keywords
-;;      '(
-;;	(,choice-keywords-regexp . font-lock-comment-face)
-;;	))
-
-;;    (defun choice-mode ()
-;;      "Major mode for editing ChoiceScript files"
-;;      (interactive)
-;;      (kill-all-local-variables)
-;;      (use-local-map choice-mode-map)
-;;      (set (make-local-variable 'font-lock-defaults) '(choice-font-lock-keywords))
-;;      (setq major-mode 'choice-mode)
-;;      (setq mode-name "ChoiceScript")
-;;      (run-hooks 'choice-mode-hook))
-    
 ;;;###autoload
-(define-derived-mode choice-mode sws-mode "Choice Mode"
+(define-derived-mode choice-mode sws-mode "ChoiceScript Mode"
   "Major mode for editing ChoiceScript files"
-  (setq mode-name "Choice Mode")
+  (setq mode-name "ChoiceScript Mode")
   (setq font-lock-defaults
 	'(
 	  (choice-font-lock-keywords)))
   )
-
 
 (provide 'choice-mode)
 
